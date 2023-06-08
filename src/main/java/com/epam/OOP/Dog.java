87f134452cf3940e4a40b6d33edd2d22874a0d47
IlyaBrutski <101987935+IlyaBrutski@users.noreply.github.com>
2023-06-08 13:34:33 +0300

    OOP

--- src/main/java/com/epam/OOP/Dog.java
+++ src/main/java/com/epam/OOP/Dog.java
@@ -1,5 +1,12 @@
 package com.epam.OOP;
 
-public class Dog {
+public class Dog extends Animal {
+    public Dog() {
+        super("brown", 4, true);
+    }
 
+    @Override
+    public String getDescription(){
+        return "This animal is mostly " + this.getColor() + ". It has " + this.getNumberOfPaws() + " " + (this.getNumberOfPaws() > 1 ? "paws" : "paw") + " and " + (this.isHasFur() ? "a" : "no") + " fur.";
+    }
 }
